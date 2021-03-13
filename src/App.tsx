bb3e3fce04db36a53eb36ee1cdc42e73c348d749
sStarkmaXx <60282152+sStarkmaXx@users.noreply.github.com>
2021-03-13 20:28:39 +0700

    create light UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.css';
 const App: React.FC = ()=> {
   return (
     <div className="App">
-      Hello TS
+      <div className="mainText">Translate this sentence</div>
+      <div className="head"></div>
+      <div className="body"></div>
+      <div className="bubble"></div>
+      <div className="line"></div>
+      <div className="wordBubble"></div>
+      <div className="emptyWordBubble"></div>
+      <div className="button">Check</div>
     </div>
   );
 }

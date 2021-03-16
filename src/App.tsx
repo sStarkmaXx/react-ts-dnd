73294a520189b43ac976573c5e9a50223e9b1ee1
sStarkmaXx <60282152+sStarkmaXx@users.noreply.github.com>
2021-03-16 21:48:50 +0700

    dnd realized but bugged

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,33 @@ let translateMass = ["Hello", "my", "name", "is", "Max"];
 const App: React.FC = () => {
   const [wordConteiners, setWordConteiners] = useState([
     { id: 1, words: [] },
-    { id: 2, words: [{ id: 1, word: "Hello" }, { id: 2, word: "my" }, { id: 3, word: "name" }, { id: 4, word: "is" }, { id: 5, word: "Max" }, { id: 5, word: "Max" }, { id: 5, word: "Max" }, { id: 5, word: "Max" }, { id: 5, word: "Max" }] }
+    { id: 2, words: [{ id: 1, word: "Hello" }, { id: 2, word: "my" }, { id: 3, word: "name" }, { id: 4, word: "is" }, { id: 5, word: "Max" }] }
   ])
 
-  const [currentWordConteiner, setCurrentWordConteiner] = useState()
+  interface IwordConteiner {
+      id:number,
+      words:{
+        id:number,
+        word:string}[]
+  }
+  interface Iitem{
+    id: number,
+    word: string
+  }
+  const [currentWordConteiner, setCurrentWordConteiner] = useState<IwordConteiner>({id:0,words:[{id:0,word:"asd"}]})
 
-  const [currentWord, setCurrentWord] = useState()
+  const [currentWord, setCurrentWord] = useState<Iitem>({id:0,word:"asd"})
 
-  function dragOverHandler(e: React.DragEvent<HTMLDivElement>): void {
-    e.preventDefault();
+  function dragOverHandler(event: React.DragEvent<HTMLDivElement>): void {
+    debugger
+    event.preventDefault();
   }
 
   function dragLeaveHandler(e: React.DragEvent<HTMLDivElement>): void {
     
   }
 
-  function dragStartHandler(e: React.DragEvent<HTMLDivElement>, wordConteiner: { id: number; words: { id: number; word: string; }[]; }, item: { id: number; word: string; }): void {
+  function dragStartHandler(e: React.DragEvent<HTMLDivElement>, wordConteiner: IwordConteiner, item: Iitem): void {
     setCurrentWordConteiner(wordConteiner)
     setCurrentWord(item)
   }
@@ -34,12 +45,39 @@ const App: React.FC = () => {
     
   }
 
-  function dropHandler(e: React.DragEvent<HTMLDivElement>, wordConteiner: { id: number; words: { id: number; word: string; }[]; }, item: { id: number; word: string; }): void {
+  
+
+  function dropHandler(e: React.DragEvent<HTMLDivElement>, wordConteiner:IwordConteiner, item: Iitem): void {
+    debugger
     e.preventDefault();
-    const currentIndex = wordConteiner.words.indexOf(item)
-    wordConteiner.words.splice(currentIndex, 1)
+    const currentIndex = currentWordConteiner.words.indexOf(currentWord)
+    currentWordConteiner.words.splice(currentIndex, 1)
     const dropIndex = wordConteiner.words.indexOf(item)
-    wordConteiner.words.splice(currentIndex, 1)
+    wordConteiner.words.splice(dropIndex+1, 0, currentWord)
+    setWordConteiners(wordConteiners.map(b=>{
+      if(b.id === wordConteiner.id){
+        return wordConteiner
+      }
+      if(b.id === currentWordConteiner.id){
+        return currentWordConteiner
+      }
+      return b
+    }))
+  }
+
+  function dropWordConteinerHandler(e: React.DragEvent<HTMLDivElement>, wordConteiner: { id: number; words: { id: number; word: string; }[]; }): void {
+    wordConteiner.words.push(currentWord)
+    const currentIndex = currentWordConteiner.words.indexOf(currentWord)
+    currentWordConteiner.words.splice(currentIndex, 1)
+    setWordConteiners(wordConteiners.map(b=>{
+      if(b.id === wordConteiner.id){
+        return wordConteiner
+      }
+      if(b.id === currentWordConteiner.id){
+        return currentWordConteiner
+      }
+      return b
+    }))
   }
 
   return (
@@ -50,7 +88,10 @@ const App: React.FC = () => {
       <div className="bubble">{mass}</div>
       <div className="dndCont">
         {wordConteiners.map(wordConteiner =>
-          <div className="wordConteiner">
+          <div 
+            onDragOver={(e) => dragOverHandler(e)}
+            onDrop={(e) => dropWordConteinerHandler(e, wordConteiner)}
+            className="wordConteiner">
             {wordConteiner.words.map(item =>
               <div
                 onDragOver={(e) => dragOverHandler(e)}
@@ -72,6 +113,6 @@ const App: React.FC = () => {
 
 export default App;
 
-function item(wordConteiners: { id: number; words: { id: number; word: string; }[]; }[], item: any): [any, any] {
-  throw new Error('Function not implemented.');
-}
+
+
+
